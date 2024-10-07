a0b9ff5061d5daeda8c0fa6246a32c9942f22a5a
Ederson Torresini <etorresini@ifsc.edu.br>
2024-10-07 20:43:28 +0000

    Usar o máximo da tela com scale

--- config.js
+++ config.js
@@ -2,5 +2,12 @@ export default {
   type: Phaser.AUTO,
   width: 800,
   height: 450,
-  parent: 'game-container'
+  parent: 'game-container',
+  scale: {
+    mode: Phaser.Scale.FIT,
+    parent: 'game-container',
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 800,
+    height: 450
+  }
 }
